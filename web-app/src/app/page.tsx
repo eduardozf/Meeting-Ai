bb765c8da24b212da31e44aa06958701ff2fbdb9
EduardoZF <eduardozf1@gmail.com>
2024-03-18 23:26:00 -0300

    + Added: Basic meet detail page + Added: Basic Shadcn components

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,112 +1,215 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { toast } from "@/components/ui/use-toast";
+import { Wand, Music2, Video, AudioLines } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
+  const form = useForm();
+
+  function onSubmit(data: any) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-6">
+      <div className="w-full flex space-x-4">
+        <div className="w-2/3 flex flex-col space-y-2">
+          <div className="w-full border-2 border-neutral-100 p-8 rounded-md space-y-8">
+            <div>
+              <h1 className="text-2xl font-bold text-gray-900">Edit Details</h1>
+              <span className="text-sm text-gray-500">Update meet details</span>
+            </div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full space-y-6"
+              >
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Video Title" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="resume"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Summary</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="A short meeting summary ..."
+                          className="resize-none"
+                          rows={6}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                      <Button type="button" className="space-x-2">
+                        <Wand className="h-4 w-4" />
+                        <span>Generate with AI</span>
+                      </Button>
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Save</Button>
+              </form>
+            </Form>
+          </div>
+          <div className="w-full border-2 border-neutral-100 p-8 rounded-md space-y-8">
+            <div>
+              <h1 className="text-2xl font-bold text-gray-900">
+                Extra information
+              </h1>
+              <span className="text-sm text-gray-500">
+                More information about the meeting
+              </span>
+            </div>
+            <div className="w-full">
+              <span>Persons</span>
+              <div className="flex space-x-2">
+                <Avatar>
+                  <AvatarImage src="" />
+                  <AvatarFallback className="bg-gray-900 text-white hover:bg-pink-900">
+                    P1
+                  </AvatarFallback>
+                </Avatar>
+                <Avatar>
+                  <AvatarImage src="" />
+                  <AvatarFallback className="bg-gray-900 text-white hover:bg-blue-900">
+                    P2
+                  </AvatarFallback>
+                </Avatar>
+                <Avatar>
+                  <AvatarImage src="" />
+                  <AvatarFallback className="bg-gray-900 text-white hover:bg-green-900">
+                    P3
+                  </AvatarFallback>
+                </Avatar>
+                <Avatar>
+                  <AvatarImage src="" />
+                  <AvatarFallback className="bg-gray-900 text-white hover:bg-yellow-900">
+                    P4
+                  </AvatarFallback>
+                </Avatar>
+              </div>
+              <span className="text-sm text-gray-500">
+                Persons can only be added when uploading a new meeting
+              </span>
+            </div>
+            <div className="w-full border-2 border-neutral-100 rounded-md flex justify-end space-x-4">
+              <Button type="button" className="space-x-2">
+                <Video className="h-4 w-4" />
+                <span>Download Video</span>
+              </Button>
+              <Button type="button" className="space-x-2">
+                <Music2 className="h-4 w-4" />
+                <span>Download Mp3</span>
+              </Button>
+              <Button type="button" className="space-x-2">
+                <AudioLines className="h-4 w-4" />
+                <span>Download Transcription</span>
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="w-1/3 border-2 border-neutral-100 rounded-md space-y-4 flex flex-col">
+          <div className="py-3 border-b-2 border-neutral-100">
+            <span className="p-2 font-bold ">Video Preview</span>
+          </div>
+          <video src="video.mkv" controls></video>
+          <div className="py-3 border-y-2 border-neutral-100">
+            <span className="p-2 font-bold ">Transcription</span>
+          </div>
+          <p className=" px-2 text-sm max-h-48 overflow-y-auto">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed
+            aliquet massa, id posuere odio. Proin quis tortor ante. Vivamus
+            mattis quis velit vitae fringilla. Class aptent taciti sociosqu ad
+            litora torquent per conubia nostra, per inceptos himenaeos.
+            Vestibulum condimentum porttitor orci non maximus. Curabitur rutrum
+            massa eu posuere mattis. Sed nec tempor est, eu vestibulum ex. Cras
+            accumsan felis dignissim dolor tristique bibendum vitae vel nisi.
+            Quisque malesuada augue eget tortor rutrum, id posuere elit rhoncus.
+            Nunc in elementum quam, ac rhoncus eros. Phasellus consectetur
+            interdum neque nec eleifend. Morbi luctus sem et nisi venenatis,
+            tempor facilisis nibh convallis. Ut ac dapibus urna. Nam pretium
+            sodales erat in sagittis. Sed ullamcorper lectus at ornare gravida.
+            Duis interdum sapien non ipsum porttitor dictum. Integer malesuada,
+            neque in efficitur tincidunt, augue nisi ullamcorper tortor,
+            faucibus egestas purus augue eleifend quam. Fusce posuere, justo at
+            scelerisque ultricies, sem dolor facilisis tortor, sed laoreet nunc
+            ante a massa. Donec rhoncus pharetra vestibulum. Morbi posuere, nunc
+            a accumsan sagittis, sapien enim aliquet lorem, in pellentesque
+            lacus nisi eget risus. Phasellus tincidunt faucibus bibendum.
+            Aliquam egestas, lectus a molestie vehicula, quam neque eleifend
+            orci, ac blandit metus nulla ut sapien. Proin posuere, diam sit amet
+            luctus dapibus, ipsum felis mattis risus, a hendrerit diam massa vel
+            metus. Aliquam erat volutpat. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Suspendisse egestas, elit sed aliquam
+            rutrum, nisl justo elementum turpis, non dapibus nisi nisl vitae
+            augue. Nam id mauris maximus, lacinia leo vitae, blandit ante.
+            Integer placerat eleifend sem non venenatis. Fusce laoreet ultrices
+            consectetur. Phasellus at dui cursus metus ornare elementum. Nullam
+            aliquam ante eros, a dapibus metus condimentum ut. Nam mollis eu leo
+            et lacinia. Cras gravida sed lorem non consequat. Fusce eu metus id
+            ante egestas luctus. Pellentesque ultricies varius dolor non
+            commodo. Integer viverra ultrices velit id congue. Nunc luctus diam
+            lacus, in consequat erat pulvinar ut. Proin placerat enim risus, eu
+            fermentum nibh varius quis. Morbi purus massa, dictum eget nunc a,
+            vulputate dictum nibh. Fusce in elementum sem. Etiam gravida ipsum
+            rutrum, venenatis orci at, euismod velit. Sed dignissim, magna
+            pellentesque sodales tincidunt, dolor nisi egestas felis, sit amet
+            euismod velit ipsum ut lacus. Aenean eget felis nec mi commodo
+            eleifend non quis nisl. Ut quis libero aliquam, tempus enim quis,
+            iaculis urna. Aliquam et orci euismod justo pretium vulputate non ut
+            nunc. Ut eu leo mauris. Pellentesque non accumsan orci, at pretium
+            nibh. In malesuada arcu risus, a varius odio egestas id. Quisque
+            viverra turpis a orci vulputate molestie. Sed fringilla augue a
+            lacus tristique, ut mollis odio faucibus.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <div className="py-3 border-y-2 border-neutral-100">
+            <span className="p-2 font-bold ">Transcription Context</span>
+          </div>
+          <p className=" px-2 text-sm max-h-28 overflow-y-auto">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed
+            aliquet massa, id posuere odio. Proin quis tortor ante. Vivamus
+            mattis quis velit vitae fringilla. Class aptent taciti sociosqu ad
+            litora torquent per conubia nostra, per inceptos himenaeos.
+            Vestibulum condimentum porttitor orci non maximus. Curabitur rutrum
+            massa eu posuere mattis. Sed nec tempor est, eu vestibulum ex. Cras
+            accumsan felis dignissim dolor tristique bibendum vitae vel nisi.
+            Quisque malesuada augue eget tortor rutrum, id posuere elit rhoncus.
+            Nunc in elementum quam, ac rhoncus eros.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
       </div>
     </main>
   );
